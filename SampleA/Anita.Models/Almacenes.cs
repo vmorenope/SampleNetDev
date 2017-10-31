162b1b1c0185e42a742be9e7782c5d13e5b4a7c9
Victor.Moreno <Victor.Moreno@outlook.com.pe>
2017-10-31 15:46:08 -0500

    Modelo Almacenes

---
+++ SampleA/Anita.Models/Almacenes.cs
@@ -0,0 +1,12 @@
+﻿namespace Anita.Models
+{
+    public class Almacenes
+    {
+        public int AlmacenId { get; set; }
+        public string Almacen { get; set; }
+        public int ExistenciaTipoId { get; set; }
+        public int EstablecimientoId { get; set; }
+        public bool EsPrincipal { get; set; }
+        public bool EsValorizado { get; set; }
+    }
+}
