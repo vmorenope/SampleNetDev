6ca6e8abec42b10a0810a4c927e6a08a7094d70a
Victor.Moreno <Victor.Moreno@outlook.com.pe>
2017-10-31 16:17:13 -0500

    .2

---
+++ SampleA/Anita.Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Anita.Repositories
+{
+    public interface IRepository : IDisposable
+    {
+        int Insert<T>(T entity) where T : class;
+        bool Update<T>(T entity) where T : class;
+        bool Delete<T>(T entity) where T : class;
+        IEnumerable<T> GetList<T>() where T : class;
+        T GetById<T>(int id) where T : class;
+    }
+}
