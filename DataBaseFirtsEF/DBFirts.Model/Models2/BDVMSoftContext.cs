a4451d7793d25dcf15f8c7030b54b397562f71aa
Victor.Moreno <Victor.Moreno@outlook.com.pe>
2017-11-10 11:30:37 -0500

    Cambios 1011

---
+++ DataBaseFirtsEF/DBFirts.Model/Models2/BDVMSoftContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DBFirts.Models.Models2
+{
+    public partial class BDVMSoftContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer(@"Server=.;Database=BDVMSoft;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {}
+    }
+}
